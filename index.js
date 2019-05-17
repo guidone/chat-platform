5a5111d476b9f4d24abce06b1818abd993bbae31
Guidone <guido.bellomo@gmail.com>
2019-05-17 09:23:45 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const ChatExpress = require('./chat-platform');
+const ContextProviders = require('./chat-context-factory');
+const ChatLog = require('./chat-log');
+
+module.exports = { ChatExpress, ContextProviders, ChatLog };
